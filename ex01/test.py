c76b3d32a8b89bf13a430501beaf8676d7a1c2ff
Paul CUVILLIER <paul.poloproduction@hotmail.fr>
2019-09-16 22:24:47 +0200

    Add test_value_0_is_not_a_digit, test_value_1_is_not_a_digit and test_not_enought_values unit tests

--- ex01/test.py
+++ ex01/test.py
@@ -10,25 +10,48 @@ class TestMainMethods(unittest.TestCase):
     def test_too_much_values(self):
         with self.assertRaises(Exception) as e:
             main.main('10;4;10')
-            # self.assertTrue("1.0 BAD INPUT: You need to enter 2 values" in e.exception)
+        self.assertTrue(e.exception[0] == 10)
+        with self.assertRaises(Exception) as e:
+            main.main('10;4678;')
         self.assertTrue(e.exception[0] == 10)
 
-    # def test_not_enought_values(self):
-    #     self.assertEqual(str(main.main("10")).find("1.0 BAD INPUT"), -1)
-    #     self.assertEqual(str(main.main("10;")).find("1.0 BAD INPUT"), -1)
+    def test_not_enought_values(self):
+        with self.assertRaises(Exception) as e:
+            main.main('65;')
+        self.assertTrue(e.exception[0] == 10)
+        with self.assertRaises(Exception) as e:
+            main.main('100')
+        self.assertTrue(e.exception[0] == 10)
+        with self.assertRaises(Exception) as e:
+            main.main('')
+        self.assertTrue(e.exception[0] == 10)
     
+    def test_value_0_is_not_a_digit(self):
+        with self.assertRaises(Exception) as e:
+            main.main('1a4;10')
+        self.assertTrue(e.exception[0] == 11)
+        with self.assertRaises(Exception) as e:
+            main.main('aaaa;10')
+        self.assertTrue(e.exception[0] == 11)
+        with self.assertRaises(Exception) as e:
+            main.main('$;10')
+        self.assertTrue(e.exception[0] == 11)
 
-    # def test_value_0_is_not_a_digit(self):
-    #     self.assertEqual(str(main.main("1a4;10")).find("1.1 BAD INPUT"), -1)
-    #     self.assertEqual(str(main.main("aaaa;10")).find("1.1 BAD INPUT"), -1)
-    #     self.assertEqual(str(main.main("$;10")).find("1.1 BAD INPUT"), -1)
-    #     self.assertEqual(str(main.main("4;10")).find("1.1 BAD INPUT"), -1)
+    def test_value_1_is_not_a_digit(self):
+        with self.assertRaises(Exception) as e:
+            main.main('308;1groh0')
+        self.assertTrue(e.exception[0] == 11)
+        with self.assertRaises(Exception) as e:
+            main.main('3480;thro')
+        self.assertTrue(e.exception[0] == 11)
+        with self.assertRaises(Exception) as e:
+            main.main('350;$')
+        self.assertTrue(e.exception[0] == 11)
 
-    # def test_value_1_is_not_a_digit(self):
-    #     #TODO
     # def test_time_equals_0(self);
     #     #TODO
     
+    #TODO: CHECK IF ENTERED ";" !
 
 if __name__ == '__main__':
     unittest.main()
