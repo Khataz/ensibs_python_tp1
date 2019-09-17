23dffd2e4d33f39b11a38ea39f413dc657bfe73a
Paul CUVILLIER <paul.poloproduction@hotmail.fr>
2019-09-17 14:29:51 +0200

    Add check for values[0] & values [1] that must be different than '' and '.'

--- ex01/main.py
+++ ex01/main.py
@@ -13,9 +13,8 @@ def main(line):
     @error
     """
     values = line.split(';')
-    if((len(values) == 2) and (values[0] != '') and (values[1] != '')):
+    if((len(values) == 2) and (values[0] != '') and (values[1] != '') and (values[0] != '.' and values[1] != '.')):
         if(re.match('^[0-9]*\.?[0-9]*$', values[0]) and re.match('^[0-9]*\.?[0-9]*$', values[1])):
-            #TODO: CHECK values[0] & values [1] must be different than '' and '.'
             time = float(values[0])
             distance = float(values[1])
             if(time > 0):
