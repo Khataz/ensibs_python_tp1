3e620e0e571eadf5e57951c8e42ab14f0259c061
Paul CUVILLIER <paul.poloproduction@hotmail.fr>
2019-09-17 15:31:01 +0200

    Fill beginning of __main__ in main.py (ex02)

---
+++ ex02/main.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+
+def main(line):
+    #TODO
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        try:
+            f = open(sys.argv[1], "r")
+        except:
+            print("Error while opening file")
+        for line in f.readlines():
+            try:
+                result = main(line.strip())
+            except Exception as exception :
+                error_code = exception.args[0]
+                error_msg = exception.args[1]
+                print(error_msg + ', error code : ' + str(error_code))
+            else :
+                print("Square root = " + str(result))
+    else:
+        print("Error: you must enter file name")
